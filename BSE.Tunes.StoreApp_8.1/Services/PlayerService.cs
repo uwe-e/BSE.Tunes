d255b1dcae86021216803a3b69d2e561fdaf0284
Uwe-E <git.bsetunes@github.com>
2014-12-24 09:57:38 +0100

    clean up

--- BSE.Tunes.StoreApp_8.1/Services/PlayerService.cs
+++ BSE.Tunes.StoreApp_8.1/Services/PlayerService.cs
@@ -226,7 +226,7 @@ namespace BSE.Tunes.StoreApp.Services
                     Guid guid = this.m_currentTrack.Guid;
                     if (guid != null && !guid.Equals(Guid.Empty))
                     {
-                        string strUrl = string.Format("{0}/api/files/{1}/", this.m_dataService.ServiceUrl, guid.ToString());
+                        string strUrl = string.Format("{0}/api/files/{1}", this.m_dataService.ServiceUrl, guid.ToString());
 
                         this.m_audioStreamDownloader = new AudioStreamDownloader(this.m_dataService);
                         this.m_audioStreamDownloader.DownloadProgessStarted += OnDownloadProgessStarted;
