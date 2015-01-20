441b28abfde331f199057e1994cef61039ec8d7d
Uwe-E <git.bsetunes@github.com>
2015-01-20 19:20:09 +0100

    System diagnostics tracing enabled

--- BSE.Tunes.WebApi2/App_Start/WebApiConfig.cs
+++ BSE.Tunes.WebApi2/App_Start/WebApiConfig.cs
@@ -5,23 +5,36 @@ using System.Web.Http;
 
 namespace BSE.Tunes.WebApi
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			System.Web.Http.Tracing.SystemDiagnosticsTraceWriter traceWriter = config.EnableSystemDiagnosticsTracing();
+			traceWriter.IsVerbose = true;
+			traceWriter.MinimumLevel = System.Web.Http.Tracing.TraceLevel.Warn;
+
+			config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
 			// Web API routes
-            config.MapHttpAttributeRoutes();
+			config.MapHttpAttributeRoutes();
 			// Web API configuration and services
 			// Configure the custom exceptions.
 			config.Filters.Add(new BSE.Tunes.WebApi.Filters.ExceptionFilter());
 			config.Services.Add(typeof(System.Web.Http.ExceptionHandling.IExceptionLogger), new BSE.Tunes.WebApi.Web.Http.TraceExceptionLogger());
-			
+
 			config.Routes.MapHttpRoute(
-                name: "DefaultApi",
+				name: "FilesApi",
+				routeTemplate: "api/files/{action}/{id}",
+				defaults: new
+				{
+					id = RouteParameter.Optional
+				}
+			);
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
 				routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
 }
